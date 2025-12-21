e8b96616a214af0b599f8588be7c83da372a61b1
TheMagicNacho <themagicnacho@protonmail.com>
2025-12-21 00:19:51 -0500

    implement core capability

--- clean-pic/src-tauri/src/lib.rs
+++ clean-pic/src-tauri/src/lib.rs
@@ -1,14 +1,175 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use image::guess_format;
+use img_parts::{ImageEXIF, ImageICC};
+use log::debug;
+use rand::prelude::*;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Error;
+use std::path::PathBuf;
+
+fn is_image(path: PathBuf) -> bool {
+    let img_extensions: HashMap<&str, bool> = [
+        ("jpg", true),
+        ("jpeg", true),
+        // ("png", true),
+        // ("gif", true),
+        // ("tiff", true),
+        // ("webp", true),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    if let Some(extension) = path.extension() {
+        if let Some(ext_str) = extension.to_str() {
+            return img_extensions.contains_key(&ext_str.to_lowercase()[..]);
+        }
+    }
+    false
 }
 
+async fn gather_files(path: &PathBuf) -> Vec<String> {
+    let mut files: Vec<String> = Vec::new();
+    match tokio::fs::read_dir(path).await {
+        Ok(mut dir) => {
+            while let Ok(Some(entry)) = dir.next_entry().await {
+                let path_buf = entry.path();
+                if is_image(path_buf.clone()) {
+                    match File::open(&path_buf) {
+                        Ok(_) => files.push(path_buf.to_str().unwrap().to_string()),
+                        Err(e) => debug!("Error opening file: {}", e),
+                    }
+                }
+            }
+        }
+        Err(e) => debug!("Error reading directory: {}", e),
+    }
+    files
+}
+
+async fn remove_metadata_jpeg(input_bytes: Vec<u8>) -> Result<Vec<u8>, Error> {
+    // Parse the JPEG structure from the byte array
+    // This does NOT decode the actual image pixels, so it's fast and lossless
+    let mut jpeg = img_parts::jpeg::Jpeg::from_bytes(input_bytes.into()).unwrap();
+
+    jpeg.set_exif(None);
+    jpeg.set_icc_profile(None);
+
+    jpeg.segments_mut().retain(|segment| {
+        match segment.marker() {
+            // --- CRITICAL FOR DECODING ---
+            // SOF0: Baseline DCT (Standard JPEG)
+            img_parts::jpeg::markers::SOF0 => true,
+            // SOF2: Progressive DCT (Progressive JPEG)
+            img_parts::jpeg::markers::SOF2 => true,
+            // DQT: Define Quantization Table (Required to decompress)
+            img_parts::jpeg::markers::DQT => true,
+            // DHT: Define Huffman Table (Required to decompress)
+            img_parts::jpeg::markers::DHT => true,
+            // DRI: Define Restart Interval (Required for error recovery/sync)
+            img_parts::jpeg::markers::DRI => true,
+            // SOS: Start Of Scan (This contains the actual compressed image data)
+            img_parts::jpeg::markers::SOS => true,
+            // APP0: JFIF Header.
+            img_parts::jpeg::markers::APP0 => true,
+            // APP14: Adobe Header. Critical for color correctness.
+            img_parts::jpeg::markers::APP14 => true,
+
+            // --- EVERYTHING ELSE IS REMOVED ---
+            // APP1 (Exif), APP2 (ICC), COM (Comments), etc.
+            _ => false,
+        }
+    });
+    let mut output_bytes = Vec::new();
+    jpeg.encoder().write_to(&mut output_bytes)?;
+
+    Ok(output_bytes)
+}
+
+// creates a directory to store new files within the given path
+async fn make_write_dir(path: &PathBuf, new_dir: &str) -> Result<PathBuf, Error> {
+    let mut new_path = path.clone();
+
+    new_path.push(new_dir);
+    println!("Creating new directory at {:?}", new_path);
+    match tokio::fs::create_dir(&new_path).await {
+        Ok(_) => Ok(new_path),
+        Err(e) => {
+            if e.kind() == std::io::ErrorKind::AlreadyExists {
+                tokio::fs::remove_dir(&new_path).await?;
+                tokio::fs::create_dir(&new_path).await?;
+                Ok(new_path)
+            } else {
+                Err(e)
+            }
+        }
+    }
+}
+
+async fn generate_filename() -> String {
+    let mut rng = rand::rng();
+
+    let name = rng.random::<u32>();
+
+    format!("{:#}", name)
+}
+
+#[tauri::command]
+async fn scrub_images(path: &str) -> Result<String, ()> {
+    let path = PathBuf::from(path);
+    // TODO: Make scrubbed_images configurable
+    let save_directory = make_write_dir(&path, "scrubbed_images").await.unwrap();
+    println!("Saving scrubbed image to {:?}", save_directory);
+    let files = gather_files(&path).await;
+    for file in files {
+        println!("Processing file: {}", &file);
+
+        let data = std::fs::read(&file).unwrap();
+        match guess_format(&data).unwrap() {
+            image::ImageFormat::Jpeg => {
+                let input_data = std::fs::read(file).unwrap();
+                let output_data = remove_metadata_jpeg(input_data).await.unwrap();
+                let new_name = generate_filename().await;
+                let new_path = format!("{}/{}.jpg", save_directory.display(), new_name);
+
+                std::fs::write(new_path, output_data).unwrap();
+            }
+            image::ImageFormat::Png => todo!("PNG"),
+            image::ImageFormat::Tiff => todo!("TIFF"),
+            _ => todo!("Other formats"),
+        };
+    }
+
+    Ok("Scrubbing complete".to_string())
+}
+
+#[tauri::command]
+fn count_images(path: &str) -> u64 {
+    println!("Counting image files in {}", path);
+    let mut counter: u64 = 0;
+    match std::fs::read_dir(path) {
+        Ok(entries) => {
+            for entry in entries.flatten() {
+                let path_buf = entry.path();
+                if is_image(path_buf) {
+                    counter += 1;
+                }
+            }
+            debug!("Found {} image files in {}", counter, path);
+            counter
+        }
+        Err(e) => {
+            debug!("Error reading directory: {}", e);
+            0
+        }
+    }
+}
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![count_images, scrub_images])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("error Ehile running tauri application");
 }
--- clean-pic/src/routes/+page.svelte
+++ clean-pic/src/routes/+page.svelte
@@ -1,156 +1,190 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
+    import {invoke} from "@tauri-apps/api/core";
+    import {open} from "@tauri-apps/plugin-dialog";
 
-  let name = $state("");
-  let greetMsg = $state("");
 
-  async function greet(event: Event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
-  }
+    // let name = $state("");
+    // let greetMsg = $state("");
+    // async function greet(event: Event) {
+    //   event.preventDefault();
+    //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    //   greetMsg = await invoke("greet", { name });
+    // }
+
+    let folderPath = $state("");
+    let numberOfFiles = $state(-1);
+
+    async function scrubDirectory(event: Event) {
+        event.preventDefault();
+        console.log("Scrubbing folder:", folderPath);
+        const result = await invoke("scrub_images", {path: folderPath});
+        console.log("Scrub result:", result);
+    }
+
+
+    async function selectFolder(event: Event) {
+        event.preventDefault();
+        const selected = await open({
+            directory: true,
+            multiple: false,
+            title: "Select a folder",
+        });
+        if (typeof selected === "string") {
+            folderPath = selected;
+            console.log("Selected folder:", folderPath);
+            numberOfFiles = await invoke("count_images", {path: folderPath});
+        } else {
+            console.log("No folder selected");
+        }
+    }
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri + Svelte</h1>
+    <h1>0w0 scrubby buddy</h1>
 
-  <div class="row">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
-  </div>
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
+    <form class="row" onsubmit={selectFolder}>
+        <button type="submit">Select Folder</button>
+    </form>
 
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
-  <p>{greetMsg}</p>
+
+    {#if folderPath}
+        {#if numberOfFiles === -1 }
+            <p>Counting images...</p>
+        {/if}
+        {#if numberOfFiles === 0 }
+            <p>No images found in the selected folder.</p>
+        {/if}
+
+        {#if numberOfFiles > 0}
+            <p>Number of images found: {numberOfFiles}</p>
+            <form class="row" onsubmit={scrubDirectory}>
+                <button type="submit">Scrub Me Daddy</button>
+            </form>
+        {/if}
+    {/if}
+
+
 </main>
 
 <style>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
+    .logo.vite:hover {
+        filter: drop-shadow(0 0 2em #747bff);
+    }
 
-.logo.svelte-kit:hover {
-  filter: drop-shadow(0 0 2em #ff3e00);
-}
+    .logo.svelte-kit:hover {
+        filter: drop-shadow(0 0 2em #ff3e00);
+    }
 
-:root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
+    :root {
+        font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+        font-size: 16px;
+        line-height: 24px;
+        font-weight: 400;
 
-  color: #0f0f0f;
-  background-color: #f6f6f6;
+        color: #0f0f0f;
+        background-color: #f6f6f6;
 
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
+        font-synthesis: none;
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-text-size-adjust: 100%;
+    }
 
-.container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
+    .container {
+        margin: 0;
+        padding-top: 10vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+    }
 
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
+    .logo {
+        height: 6em;
+        padding: 1.5em;
+        will-change: filter;
+        transition: 0.75s;
+    }
 
-.logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
-}
+    .logo.tauri:hover {
+        filter: drop-shadow(0 0 2em #24c8db);
+    }
 
-.row {
-  display: flex;
-  justify-content: center;
-}
+    .row {
+        display: flex;
+        justify-content: center;
+    }
 
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
+    a {
+        font-weight: 500;
+        color: #646cff;
+        text-decoration: inherit;
+    }
 
-a:hover {
-  color: #535bf2;
-}
+    a:hover {
+        color: #535bf2;
+    }
 
-h1 {
-  text-align: center;
-}
+    h1 {
+        text-align: center;
+    }
 
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
+    input,
+    button {
+        border-radius: 8px;
+        border: 1px solid transparent;
+        padding: 0.6em 1.2em;
+        font-size: 1em;
+        font-weight: 500;
+        font-family: inherit;
+        color: #0f0f0f;
+        background-color: #ffffff;
+        transition: border-color 0.25s;
+        box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+    }
 
-button {
-  cursor: pointer;
-}
+    button {
+        cursor: pointer;
+    }
 
-button:hover {
-  border-color: #396cd8;
-}
-button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
-}
+    button:hover {
+        border-color: #396cd8;
+    }
 
-input,
-button {
-  outline: none;
-}
+    button:active {
+        border-color: #396cd8;
+        background-color: #e8e8e8;
+    }
 
-#greet-input {
-  margin-right: 5px;
-}
+    input,
+    button {
+        outline: none;
+    }
 
-@media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
+    #greet-input {
+        margin-right: 5px;
+    }
 
-  a:hover {
-    color: #24c8db;
-  }
+    @media (prefers-color-scheme: dark) {
+        :root {
+            color: #f6f6f6;
+            background-color: #2f2f2f;
+        }
 
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-  button:active {
-    background-color: #0f0f0f69;
-  }
-}
+        a:hover {
+            color: #24c8db;
+        }
+
+        input,
+        button {
+            color: #ffffff;
+            background-color: #0f0f0f98;
+        }
+
+        button:active {
+            background-color: #0f0f0f69;
+        }
+    }
 
 </style>
